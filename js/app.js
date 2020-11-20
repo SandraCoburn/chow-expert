0dbcc596e531552832ffe11bcb9a11c6815efd55
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-20 15:58:17 -0800

    Fixed navigation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 function closeDrawer() {
   document.querySelector('.mdl-layout').MaterialLayout.toggleDrawer();
 }
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector('.nav-toggle').style.background =
+      'rgba(63, 41, 8, 0.2)';
+  } else {
+    document.querySelector('.nav-toggle').style.background = 'none';
+  }
+}
