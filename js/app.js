153549f5a9cc273a6419cc225c069913d31bc969
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-20 16:32:38 -0800

    Added scrollup button

--- js/app.js
+++ js/app.js
@@ -8,7 +8,9 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.querySelector('.nav-toggle').style.background =
       'rgba(63, 41, 8, 0.2)';
+    document.querySelector('.scrollup').style.display = 'block';
   } else {
     document.querySelector('.nav-toggle').style.background = 'none';
+    document.querySelector('.scrollup').style.display = 'none';
   }
 }
